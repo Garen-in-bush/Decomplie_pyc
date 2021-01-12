521ad80363cd4917e46e6a574fb955db7fb216f9
jiahao.li <50519434+Garen-in-bush@users.noreply.github.com>
2021-01-12 10:24:10 +0800

    Create decompile.py

---
+++ decompile.py
@@ -0,0 +1,43 @@
+import os
+import logging
+from utils import cut_file_path_suffix
+
+# _author: 李某人
+# E-mail: 594680963@qq.com
+
+
+class Decompile:
+    """
+      将指定目录下所有的pyc文件反编译为py文件
+    """
+    def __init__(self, absolute_path):
+        """
+        :param absolute_path: pyc文件存放路径
+        """
+        self.file_path = absolute_path
+
+    def run(self):
+        self.decompile_all(self.file_path)
+
+    @staticmethod
+    def decompile_all(file_path):
+        """
+        :param file_path: 绝对路径
+        :return: None
+        """
+        for file_name in os.listdir(file_path):
+            if os.path.isdir(os.path.join(file_path, file_name)):
+                Decompile.decompile_all(os.path.join(file_path, file_name))
+            else:
+                if file_name.split('.')[-1] == 'pyc':
+                    Decompile.decompile_one(os.path.join(file_path, file_name))
+
+    @staticmethod
+    def decompile_one(file_path):
+        """
+        :param file_path: 绝对路径
+        :return:
+        """
+        file_path = cut_file_path_suffix(file_path)
+        command = 'uncompyle6 -o %s.py %s.pyc' % (file_path, file_path)
+        os.system(command)
